4885ba00dc9cbecb400decb34a0bdad26894dd89
ShanifKn <shanifkaraden06@gmail.com>
2022-12-25 03:31:11 +0530

    first

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Css/Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My lst</button>
+        </div>
+        <h1 className="description">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries,
+        </h1>
+      </div>
+      <div className="fade">
+j
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
